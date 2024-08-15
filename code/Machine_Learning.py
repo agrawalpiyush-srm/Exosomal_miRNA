051119a671af0389ac12582fd4132dd42d76c5ee
Piyush Agrawal <piyusha@srmist.edu.in>
2024-08-15 20:30:30 +0530

    Update Machine_Learning.py

--- code/Machine_Learning.py
+++ code/Machine_Learning.py
@@ -1,64 +1,34 @@
 from numpy import arange
 from pandas import read_csv
-from sklearn.model_selection import RepeatedKFold
+from pandas import read_csv
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
-from sklearn.metrics import average_precision_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
+from sklearn.model_selection import cross_val_score
+from sklearn import model_selection
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
+from joblib import Parallel, delayed
+import joblib
 
-import pandas as pd
-import numpy as np
-from sklearn.svm import SVC
-from sklearn.metrics import roc_auc_score
+###### Load New dataset on which you want to predict ##########
 
-###### Loading datasets ########
-
-# Load train dataset
-train_url = '/Users/piyushagrawal/Desktop/ml_work/train.csv'
-dataframe_train = pd.read_csv(train_url, header=None)
-data_train = dataframe_train.values
-X_train, y_train = data_train[:, :-1], data_train[:, -1]
-
-# Load test dataset
-test_url = '/Users/piyushagrawal/Desktop/ml_work/test.csv'
-dataframe_test = pd.read_csv(test_url, header=None)
-data_test = dataframe_test.values
+test_url = 'ml_test.csv'
+dataframe = read_csv(test_url, header=None)
+data_test = dataframe.values
 X_test, y_test = data_test[:, :-1], data_test[:, -1]
 
-####### Support Vector Classifier ####
 
-clf = SVC(kernel='rbf', C=1, gamma=0.0001, probability=True)
-clf.fit(X_train, y_train)
-np.random.seed(786)
+##### Load the model ################
 
-# Make probability predictions
-y_pred_proba = clf.predict_proba(X_test)
+loaded_model = joblib.load('TNBC_finalized_model.sav')
 
-# Evaluate and print ROC AUC score
-roc_auc = roc_auc_score(y_test, y_pred_proba[:, 1])
-print("ROC AUC on test data: {0:0.2f}".format(roc_auc))
+# Make predictions on the test set
+y_pred_prob = loaded_model.predict_proba(X_test)[:, 1]
 
-# Print actual values and predicted probabilities
-print("Actual values:", y_test)
-print("Predicted probabilities:", y_pred_proba[:, 1])
+# Calculate AUROC
+auroc = roc_auc_score(y_test, y_pred_prob)
 
-# Create DataFrame with actual and predicted values
-results_df = pd.DataFrame({
-    'Actual': y_test,
-    'Predicted_Probabilities': y_pred_proba[:, 1]
-})
-
-Round predicted probabilities to 4 decimal places
-results_df['Predicted_Probabilities'] = results_df['Predicted_Probabilities'].round(3)
-
-Save DataFrame to TSV
-results_df.to_csv("SVM_indep_result", sep='\t', index=False)
+# Print AUROC
+print(f'AUROC: {auroc}')
