55dc263075f786b5b5196e901ccca240670983bf
Piyush Agrawal <piyusha@srmist.edu.in>
2024-08-15 19:43:37 +0530

    Update Boxplot.py

--- code/Boxplot.py
+++ code/Boxplot.py
@@ -9,10 +9,10 @@ ax= sns.boxplot(data=df, order=order, notch=True)
 ax= sns.stripplot(data=df, order=order)
 ax.set(ylim=(0,1))
 
-add_stat_annotation(ax, data=df, order=order, box_pairs=[("PathExt", "DEGs")], test='Mann-Whitney', text_format='full', loc='outside', verbose=2)
+add_stat_annotation(ax, data=df, order=order, box_pairs=[("Up", "Down")], test='Mann-Whitney', text_format='full', loc='outside', verbose=2)
 
-#ax.set_ylabel("Depmap Probability Score",fontsize=15,weight='bold')
-ax.set_xlabel("Her2",fontsize=15,weight='bold')
+ax.set_ylabel("3'UTR GC Content(%)",fontsize=15,weight='bold')
+ax.set_xlabel("Cancer Type",fontsize=15,weight='bold')
 ax.figure.set_size_inches(5,5)
 
 plt.savefig('/Users/agrawalp4/Downloads/navami/brca_project/piyush/Analysis_Based_Betweeness_Centrality/revision_comments/matched_samples_topnet_identifictaion/validation/Depmap/rep/Her2_Rep', dpi=600)
